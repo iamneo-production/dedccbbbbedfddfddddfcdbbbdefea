a20d8d6564a16cb288d8b5f38504c3f116907a40
Iamneo <techadmin@iamneo.ai>
2023-08-24 14:36:10 +0000

    Backup Commit

---
+++ Test/demo/src/test/java/com/example/RunTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IamNeoTest {
+  public static void main(String[] args) {
+    
+    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+    
+    WebDriver driver = new ChromeDriver();
+    
+    driver.manage().window().maximize();
+    
+    driver.get("http://iamneo.ai");
+    
+    String expectedTitle = "We are Hiring!";
+    String actualTitle = driver.getTitle();
+    if (actualTitle.equals(expectedTitle)) {
+      System.out.println("PASS: Page title matches expected value");
+    } else {
+      System.out.println("FAIL: Page title does not match expected value");
+    }
+    driver.navigate().to("https://www.facebook.com");
+    
+    driver.navigate().back();
+    
+    System.out.println("Current URL: " + driver.getCurrentUrl());
+    
+    driver.navigate().forward();
+    
+    driver.navigate().refresh();
+    
+    driver.quit();
+  }
+}
