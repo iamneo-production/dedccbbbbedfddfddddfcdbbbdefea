251fb35e361f0bcbae01a36edbdb9347bc751a50
Iamneo <techadmin@iamneo.ai>
2023-08-24 07:53:39 +0000

    Backup Commit

--- Test/demo/src/test/java/com/example/RunTest.java
+++ Test/demo/src/test/java/com/example/RunTest.java
@@ -7,4 +7,65 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @Cucumber.Options(tags = "~@ignore", snippets = SnippetType.CAMELCASE)
 public class RunTest {
+    import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+
+public class BrowserActionsSteps {
+
+    WebDriver driver;
+
+    @Given("I open the Chrome browser and maximize the window")
+    public void openChromeBrowserAndMaximizeWindow() {
+        System.setProperty("webdriver.chrome.driver", "localhost:4404");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @When("I navigate to {string}")
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    @Then("I print the verdict based on the title")
+    public void printVerdictBasedOnTitle() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "We are Hiring!";
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+
+    @When("I navigate back to {string}")
+    public void navigateBackTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    @And("I print the current page URL")
+    public void printCurrentPageURL() {
+        System.out.println("Current Page URL: " + driver.getCurrentUrl());
+    }
+
+    @And("I navigate forward")
+    public void navigateForward() {
+        driver.navigate().forward();
+    }
+
+    @And("I reload the page")
+    public void reloadPage() {
+        driver.navigate().refresh();
+    }
+
+    @Then("I close the browser")
+    public void closeBrowser() {
+        driver.quit();
+    }
 }
+
+}
+
--- Test/demo/src/test/resources/com/example/First.feature
+++
@@ -1,7 +0,0 @@
-@ignore
-  Feature: your first feature
-
-    Scenario: scen
-      Given John is a logged in user
-      When When John performs an action
-      Then John should have reached his goal
