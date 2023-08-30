f8523f3f6c7ce1521b11d29f6ec4a5cd40959d65
Iamneo <techadmin@iamneo.ai>
2023-08-30 12:15:03 +0000

    Backup Commit

--- Test/demo/src/test/java/com/example/RunTest.java
+++ Test/demo/src/test/java/com/example/RunTest.java
@@ -1,5 +1,9 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class IamNeoTest {
   public static void main(String[] args) {
