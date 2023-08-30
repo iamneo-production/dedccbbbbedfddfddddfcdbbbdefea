98d9fb52d837f445286524d1521f474caa3cb741
Iamneo <techadmin@iamneo.ai>
2023-08-30 12:19:59 +0000

    Backup Commit

--- Test/demo/src/test/java/com/example/RunTest.java
+++ Test/demo/src/test/java/com/example/RunTest.java
@@ -5,34 +5,45 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class IamNeoTest {
-  public static void main(String[] args) {
-    
-    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-    
-    WebDriver driver = new ChromeDriver();
-    
-    driver.manage().window().maximize();
-    
-    driver.get("http://iamneo.ai");
-    
-    String expectedTitle = "We are Hiring!";
-    String actualTitle = driver.getTitle();
-    if (actualTitle.equals(expectedTitle)) {
-      System.out.println("PASS: Page title matches expected value");
-    } else {
-      System.out.println("FAIL: Page title does not match expected value");
+public class SeleniumAssignment {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
-    driver.navigate().to("https://www.facebook.com");
-    
-    driver.navigate().back();
-    
-    System.out.println("Current URL: " + driver.getCurrentUrl());
-    
-    driver.navigate().forward();
-    
-    driver.navigate().refresh();
-    
-    driver.quit();
-  }
+
+    @Test
+    public void testScenario() {
+       
+        driver.get("http://iamneo.ai");
+
+        
+        String expectedTitle = "We are Hiring!";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Title does not match");
+
+        
+        driver.navigate().to("https://www.facebook.com");
+
+        
+        driver.navigate().back();
+
+        
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentURL);
+
+        
+        driver.navigate().forward();
+
+        
+        driver.navigate().refresh();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
